52bd6c860d2d5cac62a4d6cb52fddd3b9af8ecfb
l0st-idi0t <carlnod121@gmail.com>
2023-03-20 21:13:17 -0500

    mostly functional

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ submit.addEventListener("click", () => {
     getPay(school, lastname, job, fte).then(data => {
         // format the data into a table
         dataDiv.innerHTML = `
-            <table>
+            <table class="data">
                 <tr>
                     <th style="padding: 15px">School</th>
                     <th style="padding: 15px">Full/Part-Time</th>
@@ -26,8 +26,8 @@ submit.addEventListener("click", () => {
                     <tr>
                         <td>${person[0]}</td>
                         <td>${person[1] == 1 ? "Full-Time" : "Part-Time"}</td>
-                        <td>${person[2]}</td>
-                        <td>${person[3]}</td>
+                        <td>${"$" + person[2].toLocaleString("en-US")}</td>
+                        <td>${"$" + person[3].toLocaleString("en-US")}</td>
                         <td>${person[4]}</td>
                         <td>${person[5]}</td>
                     </tr>
@@ -42,6 +42,14 @@ async function getPay(school, lastname, job, fte) {
         method: 'GET',
         redirect: 'follow',
         mode: 'cors'
+    }
+
+    if (school.value.replace(/\s+/g, '') == "") {
+        school.value = "Default";
+    }
+
+    if (lastname.value.replace(/\s+/g, '') == "") {
+        lastname.value = "Default";
     }
 
     let url = `https://sublimetech.adamkockler.dev/${school.value}/${lastname.value}/${job.value}/${fte.value == "all" ? "all" : (fte.value == "full-time" ? 1 : 0.5)}`;
